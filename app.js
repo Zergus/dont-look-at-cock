8c05abc11a150e2506e41853057808bc437ac774
Serhi Melnik <Serhi Melnik>
2016-12-13 03:14:24 +0200

    title, favicon

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/*', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Running...');
 });
 
